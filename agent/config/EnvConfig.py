01816966e21ba02048fed2a003c1d876111672d0
chinajordan <1524178740@qq.com>
2026-01-08 16:30:48 +0800

    init variables

--- agent/config/EnvConfig.py
+++ agent/config/EnvConfig.py
@@ -3,7 +3,7 @@ import os
 from agent.util import log
 
 # config env
-DEEP_SEEK_KEY = "sk-642839b326e34ba19a8149815e9ec60b"
+DEEP_SEEK_KEY = "sk-642839b326e34bc19v8140915e9****"
 ENV = "dev"
 
 if not os.environ.get("DEEPSEEK_API_KEY"):
